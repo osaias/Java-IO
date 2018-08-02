19eee1a56f6b7e9ec5907cd0c5da499027e31be1
USUARIO <USUARIO@Ocs-Win10PC>
2018-08-02 00:19:22 -0300

    implementação da classe TesteFileWriter

---
+++ src/br/com/alura/teste/TesteFileWriter.java
@@ -0,0 +1,33 @@
+package br.com.alura.teste;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class TesteFileWriter {
+
+	public static void main(String[] args) {
+
+		try {
+			FileWriter writer = new FileWriter("Arquivo4.txt");
+			
+			Scanner scanner = new Scanner(System.in);
+			
+			while(scanner.hasNext()) {
+				writer.write(scanner.nextLine());
+				writer.write(System.lineSeparator());
+			}
+			
+			scanner.close();
+			writer.close();
+			
+		} catch (IOException e) {
+			System.out.println("Não conseguiu abrir o arquivo...");
+			e.printStackTrace();
+		}
+
+	}
+
+}
